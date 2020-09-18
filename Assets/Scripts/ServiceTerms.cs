0282618d61a8190a19398fed18cc679a13ce66f3
Ivan Paz <ivan.lucas.paz@gmail.com>
2020-09-18 19:24:55 -0300

    Botao termos de serviço

---
+++ Assets/Scripts/ServiceTerms.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceTerms : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OpenTerms()
+    {
+        Application.OpenURL("https://docs.google.com/document/d/18PmC5IyElv0lkRgdkqqCFSfWbM40dtWbuG3mw51fi3U/edit?usp=sharing");
+    }
+}
